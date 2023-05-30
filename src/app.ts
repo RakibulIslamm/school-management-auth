29f6af940d22aa19038ce940a28011db65c0ed70
MR RAKIB <rakibulislamsofficial@gmail.com>
2023-05-30 22:49:25 +0600

    [Done] Setup express, mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app:Application = express()
+
+app.use(cors());
+
+// Parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Hello from UM Server')
+})
+
+export default app;
