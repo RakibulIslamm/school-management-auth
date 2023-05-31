3c7dde2e97a0ceb8face9da0d5f2e0df1532b256
MR RAKIB <rakibulislamsofficial@gmail.com>
2023-05-31 12:55:10 +0600

    husky test

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-const app:Application = express()
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // Parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/', (req:Request, res:Response) => {
-    res.send('Hello from UM Server')
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello from UM Server')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import 'colors'
-import app from "./app";
+import app from './app'
 import config from './config/index'
 
-run().catch(err => console.log(err));
+run().catch(err => console.log(err))
 async function run() {
-    try{
-        const conn = await mongoose.connect(config.database_url as string);
-        console.log(`Database connected on host: ${conn.connection.host}`.bgBlue);
-        app.listen(config.port, () => {
-            console.log(`Server listening on port ${config.port}`)
-        })
+  try {
+    const conn = await mongoose.connect(config.database_url as string)
+    console.log(`Database connected on host: ${conn.connection.host}`.bgBlue)
+    app.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`)
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(`Database connection error: ${error.message}`.bgRed)
+    } else {
+      console.log(error)
     }
-    catch(error){
-        if(error instanceof Error){
-            console.log(`Database connection error: ${error.message}`.bgRed)
-        }
-        else{
-            console.log(error)
-        }
-    }
+  }
 }
